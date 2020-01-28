313cf049ae031f7790f514ccdd3af4b563965cd7
Aniket1999 <31628573+Aniket1999@users.noreply.github.com>
2020-01-28 19:27:47 +0530

    Update dashboard.component.ts

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    var msg = "user created";
+    let myItem = localStorage.getItem('message');
+    if (msg != myItem) {
+      this.router.navigate(['/user-form']);
+    }
   }
 
+  logout() {
+    localStorage.removeItem("message");
+    this.router.navigate(['/user-form']);
+  }
 }
